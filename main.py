272d1b11c3a274787e7ed0ff966690975428d9fe
YeghiazaryanGor <gor.yeghiazaryan.fd@gmail.com>
2022-10-12 18:17:16 +0400

    Create websocket. Add a function to calculate the amount of days passed from 2000/01/01 to this day.

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import websockets
+import asyncio
+import datetime
+from pyngrok import ngrok
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def day_number():
+    """
+        This function calculates the difference between current date
+        and 2000/01/01 to find the amount of days passed
+    """
+    today = datetime.datetime.today()
+    y = today.year
+    d = today.day
+    m = today.month
+    days_diff = 367 * y - (7 * (y + ((m + 9) / 12))) / 4 + (275 * m) / 9 + d - 730530
+    return days_diff
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+async def handler(websocket, path):
+    print("Client connected")
+    try:
+        while True:
+            await websocket.send('b')
+            await asyncio.sleep(10)
+    except websockets.exceptions.ConnectionClosed:
+        print("Client just disconnected")
+
+
+if __name__ == "__main__":
+    http_tunnel = ngrok.connect(8080, bind_tls=True)
+    print("testing url is: ", http_tunnel.public_url)
+    PORT = 8080
+    start_server = websockets.serve(handler, "localhost", PORT)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
