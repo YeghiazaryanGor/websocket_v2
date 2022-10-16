90d09577ad4e1539f1b1ec8660136964c6f99ba6
YeghiazaryanGor <gor.yeghiazaryan.fd@gmail.com>
2022-10-16 14:06:24 +0400

    Add a function that minimizes the degrees. Add a function that calculates the orbital elements of the moon. Add a function that calculates eccentric anomaly.

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import websockets
 import asyncio
 import datetime
 from pyngrok import ngrok
+from math import floor, pi, sin, cos, atan2
 
 
-def day_number():
+def calculate_day_difference():
     """
         This function calculates the difference between current date
         and 2000/01/01 to find the amount of days passed
@@ -13,15 +14,118 @@ def day_number():
     y = today.year
     d = today.day
     m = today.month
-    days_diff = 367 * y - (7 * (y + ((m + 9) / 12))) / 4 + (275 * m) / 9 + d - 730530
+    days_diff = 367 * y - (
+                ((7 * (y + ((m + 9) / 12))) / 4) + ((275 * m) / 9) + d - 730530)
     return days_diff
+
+
+def minimize_degrees(degree):
+    return degree - floor(degree / 360) * 360
+
+
+def calculate_orbital_elements(days_diff) -> dict:
+    """
+        The orbital elements of the Moon are:
+        1. Longitude Ascension Node (degrees)
+        2. Inclination (degrees)
+        3. Argument of Perigee (degrees)
+        4. Mean Distance
+        5. Eccentricity
+        6. Mean anomaly (degrees)
+    """
+    LongAscensionNode = 125.1228 - 0.0529538083 * days_diff
+    Inclination = 5.1454
+    PerigeeArg = 318.0634 + 0.1643573223 * days_diff
+    MeanDist = 60.2666
+    Eccentricity = 0.054900
+    MeanAnomaly = 115.3654 + 13.0649929509 * days_diff
+    return {
+        "LANode": minimize_degrees(LongAscensionNode),
+        "Incl": minimize_degrees(Inclination),
+        "PerArg": minimize_degrees(PerigeeArg),
+        "MeanDist": MeanDist,
+        "Ecc": Eccentricity,
+        "MeanAnomaly": minimize_degrees(MeanAnomaly)
+    }
+
+
+def calculate_eccentric_anomaly(meanAn, ecc):
+    E0 = meanAn + ((180 / pi) * ecc * sin(meanAn) * (1 + (ecc * cos(meanAn))))
+    print(E0)
+    E1 = E0 - ((E0 - (((180 / pi) * ecc * sin(E0)) - meanAn)) / (
+                1 - (ecc * cos(E0))))
+    while E1 - E0 <= 0.005:
+        print(E1)
+        E0 = E1
+        E1 = (E0 - (E0 - (((180 / pi) * ecc * sin(E0)) - meanAn))) / (
+                1 - (ecc * cos(E0)))
+    return E1
+
+
+def compute_rectangular_coordinates(eccAnomaly, ecc, meanDist):
+    """
+        This function computes the rectangular coordinates only in the
+        plane of the lunar orbit!!
+    """
+    x = meanDist * (cos(eccAnomaly) - ecc)
+    y = meanDist * ((1 - (ecc * ecc)) ** 0.5) * sin(eccAnomaly)
+    return x, y
+
+
+def convertToDistanceAndTrueAnomaly(x, y):
+    dist = ((x * x) + (y * y)) ** 0.5
+    trueAn = atan2(y, x)
+    return dist, trueAn
+
+
+def calculate_ecliptic_coordinates(dist, trueAn, perArg, LANode, incl):
+    xeclip = dist * ((cos(LANode) * cos(trueAn + perArg)) - (sin(LANode) * sin(
+        trueAn + perArg) * cos(incl)))
+    yeclip = dist * ((sin(LANode) * cos(trueAn + perArg)) + (cos(LANode) * sin(
+        trueAn + perArg) * cos(incl)))
+    zeclip = dist * sin(trueAn + perArg) * sin(incl)
+    return xeclip, yeclip, zeclip
+
+
+def convert_to_latlong(xeclip, yeclip, zeclip):
+    long = atan2(yeclip, xeclip)
+    lat = atan2(zeclip, ((xeclip * xeclip) + (yeclip * yeclip)) ** 0.5)
+    return lat, long
 
 
 async def handler(websocket, path):
     print("Client connected")
     try:
         while True:
-            await websocket.send('b')
+            days_diff = -3543
+            orb_elems = calculate_orbital_elements(days_diff)
+            ecc_anomaly = calculate_eccentric_anomaly(
+                orb_elems["MeanAnomaly"],
+                orb_elems["Ecc"]
+            )
+            rect_coord = compute_rectangular_coordinates(
+                ecc_anomaly,
+                orb_elems["Ecc"],
+                orb_elems["MeanDist"]
+            )
+            dist_trueAn = convertToDistanceAndTrueAnomaly(
+                rect_coord[0],
+                rect_coord[1]
+            )
+            eclip_coord = calculate_ecliptic_coordinates(
+                dist_trueAn[0],
+                dist_trueAn[1],
+                orb_elems["PerArg"],
+                orb_elems["LANode"],
+                orb_elems["Incl"]
+            )
+            latlong = convert_to_latlong(
+                eclip_coord[0],
+                eclip_coord[1],
+                eclip_coord[2]
+            )
+            message = str(latlong[0]) + ", " + str(latlong[1])
+            await websocket.send(message)
             await asyncio.sleep(10)
     except websockets.exceptions.ConnectionClosed:
         print("Client just disconnected")
